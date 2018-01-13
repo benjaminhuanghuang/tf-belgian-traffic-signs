75c3e331c3cce7b37b90daf77f35ba1d6e4f41bc
Benjamin Huang <bhuang@rms.com>
2018-01-12 18:22:35 -0800

    update

--- belgian-ts.py
+++ belgian-ts.py
@@ -28,3 +28,24 @@ train_data_directory = os.path.join(ROOT_PATH, 'data', "TrafficSigns/Training")
 test_data_directory = os.path.join(ROOT_PATH, 'data', "TrafficSigns/Testing")
 
 images, labels = load_data(train_data_directory)
+
+images_array = np.array(images)
+labels_array = np.array(labels)
+
+# Print the `images` dimensions
+print(images_array.ndim)
+
+# Print the number of `images`'s elements
+print(images_array.size)
+
+# Print the first instance of `images`
+images_array[0]
+
+# Print the `labels` dimensions
+print(labels_array.ndim)
+
+# Print the number of `labels`'s elements
+print(labels_array.size)
+
+# Count the number of labels
+print(len(set(labels_array)))
